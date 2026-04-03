28634c78734176b23673d4f67599cd5041e21eb8
Sneha G <1nt24is078.snehalatha@nmit.ac.in>
2026-04-03 21:45:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -164,5 +164,4 @@ def home():
 # RUN SERVER (DEPLOYMENT READY)
 # -------------------------------
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))   # 🔥 for Railway
     app.run(host="0.0.0.0", port=port, debug=False)
